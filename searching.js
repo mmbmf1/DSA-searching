ecac76604592d075dcee77be3955b5d8b2f99590
Michael <mmbmf.mm@gmail.com>
2020-03-08 13:47:07 -0500

    added comment

--- searching.js
+++ searching.js
@@ -111,3 +111,5 @@ const binarySearch = (array, value, start, end) => {
 console.log(binarySearch([3, 5, 6, 8, 11, 12, 14, 15, 17, 18], 8));
 
 // Given a sorted list 3, 5, 6, 8, 11, 12, 14, 15, 17, 18 and using the recursive binary search algorithm, identify the sequence of numbers that each recursive call will search to try and find 16.
+
+//need to complete the Queue part of the assignment to finish this
